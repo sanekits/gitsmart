fb3fb432837b72d99200f27fa54699c4d404b724
LES MATHESON <LMATHESON4@bloomberg.net>
2024-11-24 13:55:22 +0000

    Add -l arg

--- bin/git-remote-show-urls.sh
+++ bin/git-remote-show-urls.sh
@@ -5,6 +5,7 @@ scriptName="$(readlink -f "$0")"
 scriptDir=$(command dirname -- "${scriptName}")
 
 FILE_ARGS=()
+LINE_NUM=0 # -l|--line [nn]
 
 die() {
     builtin echo "ERROR($(basename ${scriptName})): $*" >&2
@@ -48,6 +49,16 @@ append_tree_branch() {
     done
 }
 
+append_line_number() {
+    while read line; do
+        if (( $LINE_NUM == 0 )); then
+            echo "${line}"
+            continue
+        fi
+        echo "${line}#L${LINE_NUM}"
+    done
+}
+
 transform_entry() {
     local branch="$1"
     local remote_name="$2"
@@ -62,12 +73,31 @@ transform_entry() {
         | match_subst_urls  \
         | sed -e "s,\.git\$,," \
         | append_tree_branch "$branch" \
-        | append_file_args
+        | append_file_args \
+        | append_line_number 
 
+}
+
+do_help() {
+    cat <<EOF   
+    Usage: git remote-show-urls.sh [options] [file ...]
+    -l|--line [nn]  : Append line number #nn to the URL
+EOF
 }
 
 main() {
     command -V git &>/dev/null || die "No git installed"
+    ex_args=()
+    while [[ -n $1 ]]; do
+        case $1 in
+            -l|--line) LINE_NUM=$2; shift 2 ;;
+            -h|--help) do_help; exit 0 ;;
+            --) break;;
+            -*) die "Unknown option: $1" ;;
+            *) ex_args+=( "$1" ); shift ;;
+        esac
+    done
+    set -- "${ex_args[@]}"  
 
     FILE_ARGS=( "$@" )
 
