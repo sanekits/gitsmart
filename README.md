ae138c32f1f44e233efe609f9417a6ba69caaae4
LES MATHESON <LMATHESON4@bloomberg.net>
2025-04-18 04:04:08 +0000

    Reconcile with template v2

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 Download and install the self-extracting setup script:
 ```
-curl -L https://github.com/sanekits/gitsmart/releases/download/0.9.3/gitsmart-setup-0.9.3.sh \
+curl -L https://github.com/sanekits/gitsmart/releases/download/0.9.5/gitsmart-setup-0.9.5.sh \
     -o ~/tmp$$.sh && bash ~/tmp$$.sh && rm ~/tmp$$.sh
 ```
 
--- bin/gitsmart-version.sh
+++ bin/gitsmart-version.sh
@@ -2,7 +2,14 @@
 
 # Running gitsmart-version.sh is the correct way to
 # get the home install path for the tool
-KitVersion=0.9.3
+KitVersion=0.9.5
+# The shellkit/ tooling naturally evolves out from under the dependent kits.  ShellkitSetupVers allows
+# detecting the need for refresh of templates/* derived files.  To bump the root version, 
+# zap all templates/* containing 'ShellkitTemplateVers' constants and changes to the corresponding dependent kits
+# Note that within templates/* there may be diverse versions in upstream shellkit, they don't all have to match,
+# but the derived copies should be sync'ed with upstream as needed.
+#shellcheck disable=2034
+ShellkitTemplateVers=2
 
 canonpath() {
     builtin type -t realpath.sh &>/dev/null && {
@@ -14,8 +21,7 @@ canonpath() {
         return
     }
     # Fallback: Ok for rough work only, does not handle some corner cases:
-    # shellcheck disable=SC2164
-    ( builtin cd -L -- "$(command dirname -- "$0")"; builtin echo "$(command pwd -P)/$(command basename -- "$0")" )
+    ( builtin cd -L -- "$(command dirname -- "$0")" || exit; builtin echo "$(command pwd -P)/$(command basename -- "$0")" )
 }
 
 Script=$(canonpath "$0")
@@ -23,5 +29,5 @@ Scriptdir=$(dirname -- "$Script")
 
 
 if [[ -z "$sourceMe" ]]; then
-    builtin printf "%s\t%s\n" "${Scriptdir}" "$KitVersion"
+    builtin printf "%s\t%s\n" "${Scriptdir}" $KitVersion
 fi
